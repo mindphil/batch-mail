e616c81b2a11e03ce954596183b9de48894d5b7e
Mindphil <philipumeadi@icloud.com>
2025-08-05 22:19:36 -0400

    tweak

--- program.py
+++ program.py
@@ -2,10 +2,7 @@ import pandas as pd
 import win32com.client as win32
 import os
 
-# Load your Excel file
 df = pd.read_excel(r'Documents\Email List.xlsx')
-
-# Start Outlook
 outlook = win32.Dispatch('outlook.application')
 
 for index, row in df.iterrows():
@@ -24,9 +21,7 @@ for index, row in df.iterrows():
     mail.CC = f"address1@email.com; ...; {cc}"
     mail.SentOnBehalfOfName = "myemail@mail.com"
     mail.Subject = "Email Subject Here"
-
-    # Let Outlook insert the signature with images/links
-    mail.GetInspector  # Loads default signature
+    mail.GetInspector
     signature = mail.HTMLBody
     mail.HTMLBody = f"""<p>Dear {name},</p>
 <p>Thank you for trying out my program! Attached, you'll find whatever attachment you linked in the spreadhseet, have a good one.</p>
